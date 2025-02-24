7fc9a552ae0805a3f82111860c82f0821c6cb4e2
WainZeng <zk0g0w@gmail.com>
2025-02-24 16:33:07 +0800

    init: 初始化提交

---
+++ src/test/java/com/wain/pichub/PicHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wain.pichub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
