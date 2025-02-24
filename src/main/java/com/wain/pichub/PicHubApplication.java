60acab512fa65745113b59822ef7a31855fea449
WainZeng <zk0g0w@gmail.com>
2025-02-24 16:32:27 +0800

    init: 初始化提交

---
+++ src/main/java/com/wain/pichub/PicHubApplication.java
@@ -0,0 +1,13 @@
+package com.wain.pichub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PicHubApplication.class, args);
+    }
+
+}
